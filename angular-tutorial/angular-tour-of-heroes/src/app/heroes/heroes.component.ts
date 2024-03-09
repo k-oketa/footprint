e6ce4efd008063b58a14dc8cbb7d98f67d3229f8
k-oketa <glamor.elk09@icloud.com>
2024-03-09 13:23:58 +0900

    tour-of-heroesの2章を完了

--- angular-tutorial/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tutorial/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+import {HEROES} from "../mock-heroes";
 import {Hero} from "../hero";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
+  heroes = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
