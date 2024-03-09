8ee5ae09ede634ca7432d5845d98648ef51ca0ad
k-oketa <glamor.elk09@icloud.com>
2024-03-09 17:00:51 +0900

    tour-of-heroesの5章を完了

--- angular-tutorial/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular-tutorial/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -3,22 +3,24 @@ import {Hero} from "../hero";
 import {HeroService} from "../hero.service";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class DashboardComponent implements OnInit {
+
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) {
   }
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+
 }
